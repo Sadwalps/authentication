84ac411a2b16a056c82c912fdbe7a5a6e14ba45a
Sadwalps <sadwalps2255@gmail.com>
2025-11-12 16:08:03 +0530

    design completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/signup' element={<Authentication />} />
+        <Route path='/signup' element={<Authentication sign={true} />} />
         <Route path='/login' element={<Authentication />} />
       </Routes>
     </>
--- src/Authentication.jsx
+++ src/Authentication.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Authentication() {
+function Authentication({ sign }) {
     return (
         <>
-        <div >
-            <h2>auth</h2>
 
-        </div>
             <div className='container-fluid' id='authenticationpage'>
                 <div className="row m-2">
                     <div className="col-md-3"></div>
-                    <div className="col-md-6 d-flex justify-content-center align-items-center" style={{height:"100vh"}}>
-                        <div className='w-100 rounded p-3 d-flex flex-column justify-content-center align-items-center bg-light'>
-                        <h2>Signup</h2>
-                        <h2>Login</h2>
-                        <input className='form-control rounded ' type="text" placeholder='Username' />
+                    <div className="col-md-6 d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
+                        <div className='w-100 rounded p-3 d-flex flex-column justify-content-center bg-light'>
+                            {sign ? <h2 className='text-center text-info' style={{ fontWeight: "bold" }}>Signup</h2> :
+                                <h2 className='text-center text-success' style={{ fontWeight: "bold" }}>Login</h2>}
+                            {sign && <input className='form-control rounded bg-secondary text-center mt-3 py-2' type="text" placeholder='Username' style={{ fontWeight: "bold" }} />}
+                            <input className='form-control rounded bg-secondary text-center mt-2 py-2' type="email" placeholder='email' style={{ fontWeight: "bold" }} />
+                            <input className='form-control rounded bg-secondary text-center mt-2 py-2' type="password" placeholder='password' style={{ fontWeight: "bold" }} />
+                            {sign ? <button className='mt-2 py-2 form-control btn btn-info rounded'>Signup</button> :
+                                <button className='mt-2 py-2 form-control btn btn-success rounded'>Login</button>}
+
+                            {sign ? <h6 className='mt-2'>Already a user? <Link to={'/login'}>Login</Link></h6> :
+                                <h6 className='mt-2'>Not a user? <Link to={'/signup'}>Signup</Link></h6>}
 
                         </div>
 
